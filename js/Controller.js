86855a0a285e6143c9d24111a4e5952242252fb5
emanlapig <emmanlapig@gmail.com>
2018-07-15 22:42:23 -0400

    desktop stylin

--- js/Controller.js
+++ js/Controller.js
@@ -3,7 +3,7 @@ var C = {
 	init: function() {
 		V.build_nav();
 		// init first page
-		pages[0].init();
+		V.go_to_page( pages[0].selector );
 		$( ".burger" ).on( 'click', V.open_menu );
 		$( ".cog" ).on( 'click', function(e) {
 			V.go_to_page( 'settings' );
