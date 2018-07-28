7fca1fede3e590aeda1aa24dfd282c5253e1571f
emanlapig <emmanlapig@gmail.com>
2018-07-28 15:19:47 -0400

    update torzu font

--- fonts/enochian/Regular/demo-files/demo.js
+++ fonts/enochian/Regular/demo-files/demo.js
@@ -26,10 +26,5 @@ document.body.addEventListener("click", function(e) {
     fontSize.addEventListener('change', updateSize, false);
     testText.addEventListener('input', updateTest, false);
     testText.addEventListener('change', updateTest, false);
-    fontSize.value = 32;
     updateSize();
-
-    testText.value = "adiaj htaoh dam ocon odriz pal egroz aaq elcic odo narmaz do ac eracaz naoav do lomaip paip at iaa apgob do lamaop dmoz pev olohol sd simapi ul mapi abos hot moh sd lipdai onog anda azrnz mzraf halip handaj mre bac gsnernoz sd rip abac aleroho mrasac masrg helobo sbrv sd idla egrp pnong liht abos neiz holbon at hammoc do zmiz aohton aaq qloh sd grplam at aarg daspzan at i ror lo-z arbos ohpnov zlac hsnal tlab dai ohog gsrov fnos lo<br><br>daioi riz pal plerv mzazo do ozlacim narmaz doqon-c ac racaz lehog uzrot nodiaj ninam qo egat sroc fort naon pal nognozo aib sihc placim do irro dsul sihc iuig hotlab egoo sbrut qlsrol atq adelcarap ohlat at gmarba amrasac hpdaip zazazi grplai mabos l viiv dlas piaaf mognoz haapv tgda";
-    updateTest();
-
 }());
